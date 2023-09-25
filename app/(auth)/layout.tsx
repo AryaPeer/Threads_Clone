c625c5a590af892bbbddfe103b024c3048da8149
Monk-Mental <85086354+AryaPeer@users.noreply.github.com>
2023-09-25 00:25:31 -0400

    Authentication Final Commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
 import '../globals.css'
+import { dark } from "@clerk/themes";
+import React from "react";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
     title: 'Threads',
     description: 'A Threads clone for my portfolio.',
 }
@@ -11,7 +14,7 @@ const inter = Inter({subsets: ["latin"]})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return(
-        <ClerkProvider>
+        <ClerkProvider appearance={{baseTheme: dark}}>
             <html lang="en">
                 <body className={'${inter.className} bg-dark-1'}>
                     <div className="w-full flex justify-center items-center min-h-screen">
