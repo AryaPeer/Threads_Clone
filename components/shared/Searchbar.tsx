1f0d459661d0574553d8ab92e2e177bf52316451
Monk-Mental <85086354+AryaPeer@users.noreply.github.com>
2023-10-01 14:25:41 -0400

    next/router -> next/navigation

--- components/shared/Searchbar.tsx
+++ components/shared/Searchbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { Input } from "../ui/input";
 
