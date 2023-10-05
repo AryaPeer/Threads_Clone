15cc9f21dce1feb5852277e42ad174b6116c4f73
Monk-Mental <85086354+AryaPeer@users.noreply.github.com>
2023-10-05 12:59:55 -0400

    Bug fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,7 +10,7 @@ import { redirect } from "next/navigation";
 
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
   const user = await currentUser();
-  if (!user) redirect("/onboarding");
+  if (!user) redirect("/sign-in");
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
